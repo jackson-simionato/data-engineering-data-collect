a94eb6eabac66b22123d3b2bf28209ad7988c516
jackson-simionato <simionato.jackson@gmail.com>
2025-01-27 15:45:19 -0300

    add comments

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-# %%
 import requests
 from bs4 import BeautifulSoup
 from tqdm import tqdm
+import pandas as pd
 
 cookies = {
 '_gid': 'GA1.2.171527606.1737985240',
@@ -30,6 +30,17 @@ headers = {
 }
 
 def get_content(url, headers=headers, cookies=cookies):
+    """
+    Fetch content from given URL using provided headers and cookies.
+    
+    Args:
+        url (str): Target URL to scrape
+        headers (dict): HTTP headers
+        cookies (dict): Browser cookies
+        
+    Returns:
+        requests.Response: Response object from the request
+    """
     response = requests.get(url, cookies=cookies, headers=headers)
 
     if response.status_code != 200:
@@ -38,11 +49,21 @@ def get_content(url, headers=headers, cookies=cookies):
     return response
     
 def get_basic_data(response):
+    """
+    Extract basic character information from the page content.
+    
+    Args:
+        response (requests.Response): Response object containing page HTML
+        
+    Returns:
+        dict: Dictionary containing character's basic information
+    """
     soup = BeautifulSoup(response.text, 'html.parser')
     td_page_content = soup.find('div', class_='td-page-content')
     paragrafo = td_page_content.find_all('p')[1]
     data = {}
 
+    # Handle different HTML structures for character info
     if len(paragrafo.find_all('em')) > 1:
         for em in paragrafo.find_all('em'):
             key, value = em.text.split(':')
@@ -60,6 +81,15 @@ def get_basic_data(response):
     return data
 
 def get_appearences_data(response):
+    """
+    Extract character appearances from the page content.
+    
+    Args:
+        response (requests.Response): Response object containing page HTML
+        
+    Returns:
+        list: List of games where the character appears
+    """
     soup = BeautifulSoup(response.text, 'html.parser')
     td_page_content = soup.find('div', class_='td-page-content')
     appearences = [li.text for li in 
@@ -71,6 +101,15 @@ def get_appearences_data(response):
     return appearences
 
 def get_character_data(url):
+    """
+    Combine basic info and appearances for a character.
+    
+    Args:
+        url (str): Character page URL
+        
+    Returns:
+        dict: Combined character data or None if fetch fails
+    """
     response = get_content(url)
 
     if response.status_code != 200:
@@ -82,6 +121,15 @@ def get_character_data(url):
     return character_data
 
 def get_character_links(url='https://www.residentevildatabase.com/personagens/'):
+    """
+    Get all character page URLs from the main character listing.
+    
+    Args:
+        url (str): Main characters page URL
+        
+    Returns:
+        list: List of character page URLs
+    """
     soup = BeautifulSoup(get_content(url).text, 'html.parser')
     page_content = soup.find('div', class_='td-page-content')
     h3s = page_content.find_all('h3')
@@ -98,15 +146,15 @@ def get_character_links(url='https://www.residentevildatabase.com/personagens/')
 
     return characters
 
-# %%
-## Lista de personagens
+# Get list of all character URLs
 characters = get_character_links()
 
 print(len(characters))
 print(characters)
-# %%
+
+# Scrape data for each character
 data_list = []
-for url in tqdm(characters[50:]):
+for url in tqdm(characters):
     print(url)
     character_data = get_character_data(url)
 
@@ -114,4 +162,11 @@ for url in tqdm(characters[50:]):
         character_data['url'] = url
         data_list.append(character_data)
 
-print(len(data_list))
+# Create DataFrame and save to parquet
+df = pd.DataFrame(data_list)
+print(df[~df['de nascimento'].isna()])
+df.to_parquet('../dados/personagens.parquet', index=False)
+
+# Save and verify data
+df_parquet = pd.read_parquet('../dados/personagens.parquet')
+print(df_parquet.head())
